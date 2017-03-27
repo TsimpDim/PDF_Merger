66b49181b5eae679b669ac9d1523606949d91cbf
TsimpDim <dimitristsimpitas@yahoo.gr>
2017-03-27 21:25:38 +0300

    Improved the UI, finalised merge functionality

--- PDF_Merger/MainWindow.xaml.cs
+++ PDF_Merger/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
 
 namespace PDF_Merger
 {
@@ -22,6 +25,8 @@ namespace PDF_Merger
     public partial class MainWindow : Window
     {
         int i = 0;
+        List<File_class> AddedPDFs = new List<File_class>(); //All the added PDFs are added here
+
         public MainWindow()
         {
             InitializeComponent();
@@ -38,18 +43,41 @@ namespace PDF_Merger
 
             foreach (string newfile in openDlg.FileNames)//For every file he chose
             {
-                this.filelist.Items.Add(new File { toMerge = true, file_id = i, file_path = newfile });//Create a new item on the "filelist" list with the correct attributes --!! toMerge should be a checkbox
+                this.filelist.Items.Add(new File_class  { toMerge = true, file_id = i, file_path = newfile });//Create a new item on the "filelist" list with the correct attributes
+                AddedPDFs.Add(new File_class { toMerge = true, file_id = i, file_path = newfile });//Add the new pdf to the array
                 i++;//Iterate the id
             }
         }
-    }
 
-    public class File //The class under which we save the files the user chooses
-    {
-        public bool toMerge { get; set; }
+         void CreateMergedPDF(object sender, RoutedEventArgs e)
+        {
+            using (FileStream stream = new FileStream(finalpdf.Text+".pdf", FileMode.Create)) //finalpdf.Text is what the user typed in the appropriate field
+            {
+                Document pdfDoc = new Document(PageSize.A4);
+                PdfCopy pdf = new PdfCopy(pdfDoc, stream);
+                pdfDoc.Open();
 
-        public int file_id { get; set; }
+                foreach (File_class newpdf in AddedPDFs) { 
+                    if(newpdf.toMerge)
+                        pdf.AddDocument(new PdfReader(newpdf.file_path));
+                }
 
-        public string file_path  { get; set; }
+
+                if (pdfDoc != null)
+                    pdfDoc.Close();
+            }
+        }
+        
+
+
+
+    public class File_class //The class under which we save the files the user chooses
+        {
+            public bool toMerge { get; set; }
+
+            public int file_id { get; set; }
+
+            public string file_path { get; set; }
+        }
     }
 }
